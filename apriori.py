fc041fb88605ff134a378b820658d2f46ca6da76
Arvind Thirumurugan <arvindthirumurugan@Arvinds-MBP.fios-router.home>
2019-09-22 18:10:42 -0400

    Two item subsets

--- apriori.py
+++ apriori.py
@@ -1,15 +1,53 @@
 import re
 import sys
 
+def getPrunedData(Items,Data,minSup):
+	pruned = []
+	for i in Items:
+		count = 0
+		for entry in Data:
+			if(i in entry):
+				count+=1
+		# print(count)
+		sup = float(count)/len(Data)
+		# print(sup)
+		if(sup>=minSup):
+			pruned.append(i)
+	return(pruned)
+
+def checkSupport(subItem,Data,minSup):
+	count = 0
+	for superset in Data:
+		result = all(elem in superset for elem in subItem)
+		if(result):
+			count+=1
+
+	sup = float(count)/len(Data)
+	if(sup>=minSup):
+		return(True)
+	else:
+		return(False)
+# Generate Two item subsets
+def generateTwoItemSubset(Items,minSup):
+	twoItem = []
+	for i in range(len(Items)):
+		for j in range(i+1,len(Items)):
+			if(Items[i][:3]!=Items[j][:3]):
+				subset = [Items[i],Items[j]]
+				if(checkSupport(subset,Data,minSup)):
+					twoItem.append([Items[i],Items[j]])
+	return(twoItem)
+
 # Reading data from given text file
 file = open("associationruletestdata.txt","r")
 filecontent = file.readlines();
 Data = []
 Items = []
 min = sys.maxsize
+minSup = float(sys.argv[1])
 for fileline in filecontent:
 	line = fileline.split('\t')
-	line[-1] = line[-1][:-1]
+	line[-1] = line[-1][:-2]
 	for i in range(0,len(line)-1):
 		#adding 'Gi_' prefix to all gene sequences
 		line[i] = "G"+str(i+1)+"_"+line[i]
@@ -18,7 +56,7 @@ for fileline in filecontent:
 for i in range(0,len(Data[0])-1):
 	#print(Data[0][i],"G"+str(i+1)+"_"+"Up")
 	if(Data[0][i] == "G"+str(i+1)+"_"+"Up"):
-		print(Data[0][i],"G"+str(i+1)+"_"+"Up")
+		# print(Data[0][i],"G"+str(i+1)+"_"+"Up")
 		Items.append(Data[0][i])
 		Items.append("G"+str(i+1)+"_"+"Down")
 	elif(Data[0][i] == "G"+str(i+1)+"_"+"Down"):
@@ -30,8 +68,19 @@ for line in Data:
 		Items.append(line[-1])
 
 # Contains all the unique items from all transactions
-print(Items)
-print("Length of Unique items:"+str(len(Items)))
+# print(Items)
+# print("Length of Unique items:"+str(len(Items)))
+
+Items = getPrunedData(Items,Data,minSup)
+# print(Items)
+# print(len(Items))
+
+twoItem = generateTwoItemSubset(Items,minSup)
+# print(twoItem)
+print(len(twoItem))
+
+
+
 
 '''
 subsets = []
